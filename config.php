f8298cf48c270862ab3dae6fe78c3508619bfa17
Ishita <ishitakumaripakur@gmail.com>
2022-04-23 23:24:08 +0530

    first commit

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+
+$db_name = "mysql:host=localhost;dbname=shop_db";
+$username = "root";
+$password = "";
+
+$conn = new PDO($db_name, $username, $password);
+// if($conn){
+//     echo "Successfully connected";
+// }
+
+?>
